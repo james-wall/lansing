c2fd434064b9c6ef2772bf51041a4e8f1ff998f7
James Wall <james.wall@wustl.edu>
2015-09-03 14:36:10 -0500

    added Advertising Purchase Agreement

--- home.php
+++ home.php
@@ -17,6 +17,12 @@
   <!-- Custom styles for this template -->
   <link href="starter-template.css" rel="stylesheet">
   <!--<script src="js/ie-emulation-modes-warning.js"></script>-->
+  <style>
+  .paragraph1 {
+    font-size: 10px;
+    text-align: left;
+  }
+</style>
 
 </head>
 
@@ -37,7 +43,7 @@
       <div id="navbar" class="collapse navbar-collapse">
         <ul class="nav navbar-nav">
           <li><a data-toggle="tab" href="#home">Home</a></li>
-          <li><a data-toggle="tab" href="#testOne">TestOne</a></li>
+          <li><a data-toggle="tab" href="#testOne">Advertising Purchase Agreement</a></li>
           <li><a data-toggle="tab" href="#testTwo">TestTwo</a></li>
           <li><a data-toggle="tab" href="#testThree">TestThree</a></li>
         </ul>
@@ -55,31 +61,182 @@
 
     <div id="testOne" class="tab-pane fade">
       <div class="starter-template">
-          <h1> You are now in Test One! </h1>
+        <h1> Advertising Purchasing Agreement </h1>
+        <div align="center" id="purchAgree">
+          <fieldset style="width=30%">
+            <table class = "table" border="0">
+              <form method="post">
+                <tbody>
+                  <tr>
+                    <td><input type="checkbox" value="valSheet">VALUE SHEET <br>
+                      RATE (PRICE/THOUSAND): <input type="text" name="valRate" id="valRate"></td>
+                      <td><input type="checkbox" value="menuInsert">MENU INSERT <br>
+                        RATE (PRICE/THOUSAND): <input type="text" name="menuRate" id="menuRate"></td>
+                      </tr>
+                    </tbody>
+                  </form>
+                </table>
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>Date (Week of)</th>
+                      <th>Zone/City</th>
+                      <th>QUANTITY</th>
+                      <th>COST</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><input type="text" name="date1" id="date1"></td>
+                      <td><input type="text" name="zone1" id="zone1"></td>
+                      <td><input type="text" name="quant1" id="quant1"></td>
+                      <td><input type="text" name="cost1" id="cost1"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date2" id="date2"></td>
+                      <td><input type="text" name="zone2" id="zone2"></td>
+                      <td><input type="text" name="quant2" id="quant2"></td>
+                      <td><input type="text" name="cost2" id="cost2"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date3" id="date3"></td>
+                      <td><input type="text" name="zone3" id="zone3"></td>
+                      <td><input type="text" name="quant3" id="quant3"></td>
+                      <td><input type="text" name="cost3" id="cost3"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date4" id="date4"></td>
+                      <td><input type="text" name="zone4" id="zone4"></td>
+                      <td><input type="text" name="quant4" id="quant4"></td>
+                      <td><input type="text" name="cost4" id="cost4"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date5" id="date5"></td>
+                      <td><input type="text" name="zone5" id="zone5"></td>
+                      <td><input type="text" name="quant5" id="quant5"></td>
+                      <td><input type="text" name="cost5" id="cost5"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date6" id="date6"></td>
+                      <td><input type="text" name="zone6" id="zone6"></td>
+                      <td><input type="text" name="quant6" id="quant6"></td>
+                      <td><input type="text" name="cost6" id="cost6"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date7" id="date7"></td>
+                      <td><input type="text" name="zone8" id="zone7"></td>
+                      <td><input type="text" name="quant7" id="quant7"></td>
+                      <td><input type="text" name="cost7" id="cost7"></td>
+                    </tr>
+                    <tr>
+                      <td><input type="text" name="date8" id="date8"></td>
+                      <td><input type="text" name="zone8" id="zone8"></td>
+                      <td><input type="text" name="quant8" id="quant8"></td>
+                      <td><input type="text" name="cost8" id="cost8"></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <blockquote class = "paragraph1">
+                  The Advertiser named above agrees to honor the specified literatures as per its terms and conditions to each recipient and not to hold LANSING SALES & MARKETING SERVICES 
+
+                  CORP. liable for such offer made regardless of response to design. The Advertiser authorizes LANSING SALES & MARKETING SERVICES CORP. to prepare and distribute the above 
+
+                  specific literature along with other advertisers through the United States Postal Service and the World Wide Web. The Advertiser represents that he/she possesses the right to publish, 
+
+                  and hereby grants LANSING SALES & MARKETING SERVICES CORP. and any of its publishing agents the irrevocable right and permission to reproduce (for the Advertiser), all trade-
+
+                  marks, trade names, copyrighted material and other images provided by the Advertiser to LANSING SALES & MARKETING SERVICES CORP.. The Advertiser assumes full 
+
+                  responsibility that Advertiser’s advertisement, business operations, services and products comply with all applicable laws. The Advertiser acknowledges and agrees that similar and 
+
+                  competitive advertisement may be included in the same envelope mailed by LANSING SALES & MARKETING SERVICES CORP. The FIRST TWO MAILINGS are NOT cancellable. Any 
+
+                  mailing thereafter may be cancelled by either party prior to 30 days from each mailing date.  Buyout fee will apply.
+
+                  The Advertiser acknowledges that he/she has no right or ownership in any photograph, graphic or other image provided or arranged by LANSING SALES & MARKETING SERVICES 
+
+                  CORP. for the Advertiser, including any photo or image containing the Advertiser’s picture. The Advertiser hereby releases and discharges LANSING SALES & MARKETING SERVICES 
+
+                  CORP. and any of its publishing agents from any and all claims and liability arising out of the publication of the Advertiser’s advertisement and any material provided by Advertiser to 
+
+                  LANSING SALES & MARKETING SERVICES CORP.. Each ad will be designed as instructed by the Advertiser. Upon proofing, corrections will be made at no charge and changes will be 
+
+                  made for a fee. For repeating ads, LANSING SALES & MARKETING SERVICES CORP. will update expiration dates and produce materials to meet the agreement with either implied or 
+
+                  explicit proof approval. The rates above are subject to change in the event of Postal increases.
+
+                  The Advertiser agrees that LANSING SALES & MARKETING SERVICES CORP. ’S liability to its advertiser shall not exceed that amount paid by the Advertiser to LANSING SALES & 
+
+                  MARKETING SERVICES CORP. under this agreement. Any substantial errors in LANSING SALES & MARKETING SERVICES CORP.’S production of  the Advertiser’s ad, that reduces 
+
+                  the value by over 50%, as determined by LANSING SALES & MARKETING SERVICES CORP. will be corrected at the next mailing date -- no cash refunds will be made. Color and 
+
+                  registration are not exact. In the event it becomes necessary for LANSING SALES & MARKETING SERVICES CORP., at its option, to place this agreement in the hands of an attorney or 
+
+                  collection agency for purposes of debt collection the Advertiser agrees to pay additional sum equal to cost of collection, including but not limited to , attorney’s fee or collection agent’s 
+
+                  fee. A $35.00 SERVICES CHARGE WILL BE IMPOSED FOR ANY CHECK RETURNED FOR ANY REASON. LANSING SALES & MARKETING SERVICES CORP. reserves the right to 
+
+                  cancel any advertiser and /or design(s) and to make full refund of any money paid. I understand that no agreements shall be made in writing or verbally with representative or agent of 
+
+                  LANSING SALES & MARKETING SERVICES CORP., pertaining to this agreement, unless so stated in this agreement and approved by LANSING SALES & MARKETING SERVICES 
+
+                  CORP.
+                </blockquote>
+                <table class="table">
+                  <tbody>
+                    <tr>
+                      <td>Advertiser's Electronic Signature <input type="text" name="sig" id="sig"></td>
+                      <td>Date  <input type="text" name="date" id="date"></td>
+                    </tr>
+                    <tr>
+                      <td>Title  <input type="text" name="title" id="title"></td>
+                      <td>Business Name  <input type="text" name="bizName" id="bizName"></td>
+                    </tr>
+                    <tr>
+                      <td>Street Address  </td><td> <input type="text" name="address" id="address"></td>
+                    </tr>
+                    <tr>
+                      <td>City</td><td> <input type="text" name="city" id="city"></td>
+                      <td>State</td><td> <input type="text" name="state" id="state"></td>
+                      <td>ZIP Code</td><td> <input type="text" name="zip" id="zip"></td>
+                    </tr>
+                    <tr>
+                      <td>Phone Number</td><td> <input type="text" name="phoneNum" id="phoneNum"></td>
+                      <td>Email</td><td> <input type="text" name="email" id="email"></td>
+                    </tr>
+                    <tr>
+                      <td><input id="adAgree" type="submit" value="Submit"></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </fieldset>
+              <!--<script src="/sittingDuckLogin/modules/home/views/chngVen.js"></script>-->
+            </div>
+          </div>
+        </div>
+
+
+        <div id="testTwo" class="tab-pane fade"> 
+          <div class="starter-template">
+            <h1> You are now in Test Two! </h1>
+          </div>
+        </div>
+
+
+        <div id="testThree" class="tab-pane fade">
+          <div class="starter-template">
+            <h1> You are now in Test Three! </h1>
+          </div>
+        </div>
+
       </div>
-    </div>
 
 
-    <div id="testTwo" class="tab-pane fade"> 
-      <div class="starter-template">
-        <h1> You are now in Test Two! </h1>
-      </div>
-    </div>
+      <div class="container">
+      </div><!-- /.container -->
 
-
-    <div id="testThree" class="tab-pane fade">
-      <div class="starter-template">
-          <h1> You are now in Test Three! </h1>
-      </div>
-    </div>
-
-  </div>
-
-
-  <div class="container">
-  </div><!-- /.container -->
-
-  <script>$('.nav a').on('click', function(){
+      <script>$('.nav a').on('click', function(){
             $(".btn-navbar").click(); //bootstrap 2.x
             $(".navbar-toggle").click() //bootstrap 3.x by Richard
           });</script>
